9e58fe42bf4e137128061cc88e97315cfbbf3844
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-01-05 17:54:34 -0500

    create post feature

--- blog.php
+++ blog.php
@@ -1,30 +1,47 @@
 <?php
 session_start();
 
+//check if user is logged
 if(isset($_SESSION['userId'])){
   require('./config/db.php');
   $userId = $_SESSION['userId'];
 
+  //get user id
   $stmt = $pdo -> prepare('SELECT * from users WHERE id = ?');
   $stmt -> execute([$userId]);
   $user = $stmt -> fetch();
 
+  //get posts
   $stmt = $pdo -> prepare('SELECT * from blog');
   $stmt -> execute();
   $posts = $stmt -> fetchAll();
+
+  if(isset($_POST['createPost'])){
+    $title = filter_var($_POST['postTitle'], FILTER_SANITIZE_STRING);
+    $body = filter_var($_POST['postBody'], FILTER_SANITIZE_STRING);
+
+    $stmt = $pdo -> prepare('INSERT INTO blog(title, body) VALUES(?, ?)');
+    $stmt -> execute([$title, $body]);
+    header('Location: http://localhost:8888/login-project-php/blog.php');
+  }
+
 
 }
 ?>
 
 <?php require('./inc/header.html')?>
-<?php require('./inc/newPost.html')?>
+<?php if($user -> role == "moderator" || $user -> role =="admin"){
+  require('./inc/newPost.html');
+}?>
 
 <div class="container">
+  <?php if($user -> role == "moderator" || $user -> role =="admin"){?>
   <div class="row">
     <div class="col-sm-12">
       <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Create New Post</button>
     </div>
   </div>
+  <?php }?>
   <?php foreach($posts as $post){ ?>
   <div class="card">
     <div class="card-header bg-ligh mb-3"><?php echo $post -> title ?></div>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log("it's working");
 
 if(document.getElementById("createPostModal")){
-  document.getElementById("createPostModal").addEventListener('click',function(){
-    document.getElementById("createPostFrom").click();
+  document.getElementById("createPostModal").addEventListener('click', function(){
+    document.getElementById("createPostForm").click();
   });
 }
